d8ef1c14ef74355853420d27e4a6b57f99906e98
reza rastegar <moidreza82@gmail.com>
2023-12-06 15:49:49 +0330

    Feature: Only Persian and Arabic texts are RTL

--- assets/script/addContent.js
+++ assets/script/addContent.js
@@ -1,7 +1,37 @@
 addStyle = () => {
   let domStyle = document.createElement("style");
   domStyle.id = "custom-styles";
-  domStyle.append("main { direction: rtl !important; } pre { direction: ltr;}");
+  domStyle.append(`
+    main { direction: rtl !important; }
+    pre { direction: ltr !important; }
+    .rtl { direction: rtl !important; }
+    .ltr { direction: ltr !important; }
+  `);
   document.body.appendChild(domStyle);
+
+  function detectTextDirection(element) {
+    const text = element.textContent;
+
+    if (
+      /[\u0600-\u06FF\u0750-\u077F\u0590-\u05FF\uFB50-\uFDFF\uFE70-\uFEFF]/.test(
+        text
+      )
+    ) {
+      element.classList.add("rtl");
+    } else {
+      element.classList.add("ltr");
+    }
+  }
+
+  const paragraphElements = document.querySelectorAll("p");
+  paragraphElements.forEach((element) => {
+    detectTextDirection(element);
+  });
+
+  const paragraphElementDiv = document.querySelectorAll(".text-base");
+  paragraphElementDiv.forEach((element) => {
+    detectTextDirection(element);
+  });
 };
+
 addStyle();
