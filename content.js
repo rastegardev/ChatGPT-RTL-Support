921bc4bf3f28e1b52267aa436e111e3921b88011
reza rastegar <moidreza82@gmail.com>
2024-05-02 10:49:15 +0330

    Update: change font to Estedad

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 let addFontFamily = document.createElement("style");
-addFontFamily.id = "custom-styles-font-family";
+addFontFamily.id = "custom-font-family-cdn";
 addFontFamily.append(`
-@import url('https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css');
-  * { font-family: 'VazirMatn', sans-serif !important; }
+@import url('https://v1.fontapi.ir/css/Estedad');
+  * { font-family: Estedad, sans-serif !important; };
 `);
 document.body.appendChild(addFontFamily);
