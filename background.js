2632abe240fd09c4834658122d050c031d24b520
reza rastegar <moidreza82@gmail.com>
2024-05-07 01:13:52 +0330

    Update: small improvements

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
 // Listen for the extension installation event
 chrome.runtime.onInstalled.addListener(function (details) {
   if (details.reason === "install") {
-    // Open a new tab with the Github URL
+    // Open a new tab with a specific URL when the extension is installed
     chrome.tabs.create({
-      url: "https://rastegar.info/chatgpt-rtl-support/",
+      url: "https://rastegar.info/chatgpt-rtl-support/?utm_term=install&utm_source=chromewebstore",
+    });
+  } else if (details.reason === "update") {
+    // If the extension is updated, open a different URL
+    chrome.tabs.create({
+      url: "https://rastegar.info/chatgpt-rtl-support/#whats-new",
     });
   }
 });
